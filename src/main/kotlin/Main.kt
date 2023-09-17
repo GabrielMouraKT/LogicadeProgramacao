554ea118827ff63c61604303a0760d3248deea2d
GabrielMouraKT <gabrieljknd@gmail.com>
2023-09-17 00:57:12 -0300

    Update

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,68 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+/*fun main() {
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    println("Hello Kotlin, goodbye Java!")
+
+}*/
+
+/*fun main() {
+    println("Hello Wolrd!")
+    var nome = "Gabriel" //var nome: String = "Gabriel"
+    println(nome)
+    val cpf: String = "000.000.000-00"// val variavel imutavel
+    println(cpf)
+    //fun palavra reservada para criar funcoes.
+}*/
+
+/*fun main (){
+    print("Hello")
+    print("World")
 }
+HelloWorld
+println("Hello")
+println("World")
+Hello
+World*/
+
+
+/*fun main (){
+    // var é o armario String é a etiqueta aqui esta definindo o tipo da variavel entao nao altera
+    var apelido: String = ""
+    var apelido2 = 45
+    val documento = "000.000.000-00"
+}*/
+
+/*fun main (){
+    println("Digite seu nome: ")
+    val nome: String = readln()
+    println("Seja bem vindo $nome")//metodo ideal de concatenacao(interpolação)
+    println("Seja bem vindo " + nome)// metodo de concatenação igual o java
+    println("Seja bem vindo ${nome.uppercase()}")//concatenando e deixando tudo maiuscula
+    println("Seja bem vindo ${nome.lowercase()}")//concatenando e deixando tudo minusculo
+
+}*/
+/*fun main (){
+    println("Escreva seu nome: ")
+    val nome: String = readln()
+    println("Digite sua idade: ")
+    val idade: Int = readln().toIntOrNull() ?:32
+    println(nome)
+    println(idade)
+    println("Seja bem vindo $nome " + "Sua idade é $idade")
+}*/
+        //TIPOS E VARIAVEIS
+    //Para números inteiros temos a seguinte divisão:
+    //
+    //Tipo	Tamanho	Menor valor possível	Maior valor possível
+    //Byte	8 bits	-128	127
+    //Short	16 bits	-32768	32767
+    //Int	32 bits	-2,147,483,648	2,147,483,647
+    //Long	64 bits	-9,223,372,036,854,775,808	9,223,372,036,854,775,807
+    //Enquanto para números fracionários temos esta:
+    //
+    //Tipo	Quantidade de dígitos inteiros	Quantidade de dígitos decimais
+    //Float	8	6
+    //Double	11	15
+    fun main (){
+
+
+    }
